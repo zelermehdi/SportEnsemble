0288e73570c6e0f2783f141a5f6d90b7ae1f97e7
zelermehdi <gamouh.mehdi@gmail.com>
2025-03-06 16:53:33 +0100

    MàJ flash-messages.blade.php: corrections des messages flash

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -1,17 +1,29 @@
 @if (session('success'))
-    <div class="mb-4 p-3 rounded-lg bg-green-100 text-green-800">
+    <div x-data="{ show: true }" 
+         x-show="show" 
+         x-init="setTimeout(() => show = false, 3000)" 
+         class="mb-4 p-3 rounded-lg bg-green-100 text-green-800 border border-green-400 shadow-md transition transform duration-500 ease-in-out"
+         role="alert">
         {{ session('success') }}
     </div>
 @endif
 
 @if (session('error'))
-    <div class="mb-4 p-3 rounded-lg bg-red-100 text-red-800">
+    <div x-data="{ show: true }" 
+         x-show="show" 
+         x-init="setTimeout(() => show = false, 3000)" 
+         class="mb-4 p-3 rounded-lg bg-red-100 text-red-800 border border-red-400 shadow-md transition transform duration-500 ease-in-out"
+         role="alert">
         {{ session('error') }}
     </div>
 @endif
 
 @if (session('warning'))
-    <div class="mb-4 p-3 rounded-lg bg-yellow-100 text-yellow-800">
+    <div x-data="{ show: true }" 
+         x-show="show" 
+         x-init="setTimeout(() => show = false, 3000)" 
+         class="mb-4 p-3 rounded-lg bg-yellow-100 text-yellow-800 border border-yellow-400 shadow-md transition transform duration-500 ease-in-out"
+         role="alert">
         {{ session('warning') }}
     </div>
 @endif
